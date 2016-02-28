8a500286029c8ac4a48e7a867d7c6f3a56df44fa
juned.khan <juned3you@gmail.com>
2016-02-28 09:34:29 +0530

    Added CORS headers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,29 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    
+    /**
+	 * Add CORS headers.
+	 * 
+	 * @return
+	 */
+	public Result checkPreFlight() {
+		response().setHeader("Access-Control-Allow-Origin", "*");
+		response().setHeader("Access-Control-Allow-Methods", "POST");
+		response()
+				.setHeader(
+						"Access-Control-Allow-Headers",
+						"accept, origin, Content-type, x-json, x-prototype-version, x-requested-with, apiKey, X-Auth-Token");
+		return ok();
+	}
+
+	/**
+	 * Add CORS before any call.
+	 * 
+	 * @param all
+	 * @return
+	 */
+	public Result preFlight(String all) {
+		return checkPreFlight();
+	}
 }
