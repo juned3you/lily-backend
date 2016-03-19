482bfa2b2a0e538abe35db739a1ba5c1a06fe3eb
juned.khan <juned3you@gmail.com>
2016-03-19 13:06:45 +0530

    Id issue

--- app/com/lily/models/BaseEntity.java
+++ app/com/lily/models/BaseEntity.java
@@ -3,18 +3,15 @@ package com.lily.models;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @MappedSuperclass
 public abstract class BaseEntity {
-	
-	@Id	
+
 	public Integer id;
-	
-	
+
 	@JsonIgnore
 	@Column(name = "created")
 	public Date createdAt;
